921657bb4f17a0a1c69a0837259afedc93eb9fb8
Deep nimbark <143580597+deepnimbark05@users.noreply.github.com>
2024-11-18 09:09:36 +0530

    Describe what you updated or changed

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       // initialRoute: '/profile',
       home: SplashScreen(),
       getPages: [
